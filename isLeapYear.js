3645da639345becd68cece2d51f04fedeefd733f
Sandra Atkinson <100942604+SandraAcko@users.noreply.github.com>
2023-09-02 20:51:57 +0100

    Update isLeapYear.js
    Tutor's solution to challenge commented in.
    I need to nest my if statements better rather than combining too many conditional comparators

--- isLeapYear.js
+++ isLeapYear.js
@@ -16,3 +16,21 @@ function isLeapYear(year) {
 
 var leapCalc = isLeapYear(prompt("Enter a year and I will tell you if it is a leap year!"));
 alert(leapCalc);
+
+// Solution from following lesson: (must nest if statements better)
+
+// function isLeap(year) {
+//     if (year % 4 === 0) {
+//         if (year % 100 === 0) {
+//             if (year & 400 === 0) {
+//                 return "Leap year."
+//             } else {
+//                 return "Not a leap year."
+//             }
+//         } else {
+//             return "Leap year."
+//         }
+//     } else {
+//         return "Not a leap year."
+//     }
+// }
